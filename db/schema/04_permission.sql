6a3464a2001c5dc277da97114fce157fd3335629
Nathan Welch <nathanwelch_@hotmail.com>
2019-07-06 16:15:35 +1000

    Add permissions
    * map of services => service_permissions
    * included in JWT as "permissions"

--- db/schema/04_permission.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS permission (
-    permission_id SERIAL  PRIMARY KEY NOT NULL,
-    user_id       INTEGER NOT NULL REFERENCES users(user_id),
-    role_id       INTEGER NOT NULL REFERENCES role(role_id)
-);
